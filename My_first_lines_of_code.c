ee589fe5521a02033c74911b59fae963700a97ac
EmmanuelDomenech <82624469+EmmanuelDomenech@users.noreply.github.com>
2021-04-15 23:23:27 -0400

    Create My_first_lines_of_code.c

---
+++ My_first_lines_of_code.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ 
+int main(void) {
+   printf("Hello: World\n");
+   printf("Will you like some Coffe\b\n");
+   printf("please choose tea\r");
+   printf("Will you like some Tea?\n");
+   printf("I am sorry, I am 'out' of Tea\t\n");
+   printf("World, are you going to wait for that Tea\?");
+
+   return 0;
+}
+/*
+Line number 4 “\n” is creating a new line; it gives me a vertical tab and moves me to the next line.
+
+Line number 5 “\b\n\” while \b moves the cursor one position back \n is creating a new line.
+
+Line number 6 “\r” made this line from the IN/OUT Panel disappear.
+
+Line number 7 “\n” is creating a new line; it gives me a vertical tab and moves me to the next line.
+
+Line number 8 “\t\n\” while \t gives horizontal tab \n is creating a new line; it gives me a vertical tab and moves me to the next line
+
+Line number 9 “\?” add the question mark at the end of the line of code. 
+/*
