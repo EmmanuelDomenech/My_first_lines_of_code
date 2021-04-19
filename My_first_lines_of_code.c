9b8fe1131a80606868cd576a0ea2a036910d4b58
EmmanuelDomenech <82624469+EmmanuelDomenech@users.noreply.github.com>
2021-04-19 13:55:46 -0400

    Update My_first_lines_of_code.c

--- My_first_lines_of_code.c
+++ My_first_lines_of_code.c
@@ -10,7 +10,7 @@ int main(void) {
 
    return 0;
 }
-/* Actual date I wrote My first code in C: June 23, 2020
+/* Notes:
 
 Line number 4 “\n” is creating a new line; it gives me a vertical tab and moves me to the next line.
 
